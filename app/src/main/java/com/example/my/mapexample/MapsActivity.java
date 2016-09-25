4f62e52c15dfc69e30ff997766a5de00a9b02e3b
toto3138 <to-31@hotmail.com>
2016-09-26 00:25:05 +0700

    Change permission scheme to support API 23

--- app/src/main/java/com/example/my/mapexample/MapsActivity.java
+++ app/src/main/java/com/example/my/mapexample/MapsActivity.java
@@ -1,13 +1,19 @@
 package com.example.my.mapexample;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,28 +25,57 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.jar.*;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener
+{
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        else
+        {
+            initMap();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+       if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                initMap();
+            }
+            else
+            {
+                Toast.makeText(this, "Access location permission need to be granted in order to show current location.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void initMap()
+    {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
-        //Check if location is available
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) //Check if location is available
+        {
             Criteria criteria = new Criteria();
             criteria.setAccuracy(Criteria.ACCURACY_FINE);
             criteria.setAltitudeRequired(false);
@@ -51,10 +86,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             String provider = locationManager.getBestProvider(criteria, true);
             try
             {
-                Location location = locationManager.getLastKnownLocation(provider);
-                updateMarker(location);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude())));
-                locationManager.requestLocationUpdates(provider, 3000, 10, this);
+                locationManager.requestLocationUpdates(provider, 0, 0, this);
             } catch (SecurityException ex) {ex.printStackTrace();}
         }
     }
@@ -66,8 +98,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude())));
     }
     @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-    }
+    public void onStatusChanged(String s, int i, Bundle bundle) {}
     @Override
     public void onProviderEnabled(String s) {}
     @Override
